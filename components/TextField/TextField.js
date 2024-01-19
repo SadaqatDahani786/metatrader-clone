14b27466eb2b3b96aabb8314a817c3d97be22811
Sadaqat Ali <46186868+SadaqatDahani786@users.noreply.github.com>
2024-01-19 23:42:22 +0500

    Added multiple form components such as FormLabel, FormGroup and TextField.

---
+++ components/TextField/TextField.js
@@ -0,0 +1,59 @@
+import { TextInput, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+//Components
+import FormGroup from "../FormGroup";
+import FormLabel from "../FormLabel/";
+
+/*
+ ** ** =============================================================
+ ** ** ** Component [TextField]
+ ** ** =============================================================
+ */
+const TextField = ({
+  icon,
+  defaultValue,
+  value,
+  onChangeText,
+  label,
+  placeholder,
+  type,
+}) => {
+  return (
+    <FormGroup>
+      {label && <FormLabel text={label} />}
+      <TextInput
+        style={styles.textField}
+        defaultValue={defaultValue}
+        value={value}
+        onChangeText={onChangeText}
+        placeholder={placeholder}
+        secureTextEntry={type === "password"}
+        keyboardType={
+          type === "email"
+            ? "email-address"
+            : type === "number"
+            ? "number-pad"
+            : ""
+        }
+      />
+      {icon && <Feather name={icon} size={20} color="hsl(0, 0%, 40%)" />}
+    </FormGroup>
+  );
+};
+
+/*
+ ** **
+ ** ** ** Styles
+ ** **
+ */
+const styles = StyleSheet.create({
+  textField: {
+    fontSize: 16,
+    padding: 8,
+    flexGrow: 1,
+    textAlign: "right",
+  },
+});
+
+export default TextField;
