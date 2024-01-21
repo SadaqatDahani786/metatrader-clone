8e950e6858f123ab57838ceecfbcbdad4d4bedc9
Sadaqat Ali <46186868+SadaqatDahani786@users.noreply.github.com>
2024-01-21 21:07:18 +0500

    Added props "color" and "alignLeft" to TextField component and also type "search" support.

--- components/TextField/TextField.js
+++ components/TextField/TextField.js
@@ -19,26 +19,44 @@ const TextField = ({
   placeholder,
   type,
   border = true,
+  alignLeft = false,
+  color = "BLACK",
 }) => {
   return (
     <FormGroup delegatedStyles={{ borderBottomWidth: border ? 1 : 0 }}>
-      {label && <FormLabel text={label} />}
+      {icon && alignLeft && (
+        <Feather name={icon} size={20} color="hsl(0, 0%, 40%)" />
+      )}
+      {label && !alignLeft && <FormLabel text={label} />}
       <TextInput
-        style={styles.textField}
+        style={[
+          styles.textField,
+          {
+            color: color === "BLACK" ? "black" : "white",
+            textAlign: alignLeft ? "left" : "right",
+          },
+        ]}
+        placeholderTextColor={
+          color === "BLACK" ? "hsl(0, 0%, 40%)" : "hsl(0, 0%, 80%)"
+        }
         defaultValue={defaultValue}
         value={value}
         onChangeText={onChangeText}
         placeholder={placeholder}
         secureTextEntry={type === "password"}
+        returnKeyType={type === "search" ? "search" : "default"}
         keyboardType={
           type === "email"
             ? "email-address"
             : type === "number"
             ? "number-pad"
-            : ""
+            : "default"
         }
       />
-      {icon && <Feather name={icon} size={20} color="hsl(0, 0%, 40%)" />}
+      {icon && !alignLeft && (
+        <Feather name={icon} size={20} color="hsl(0, 0%, 40%)" />
+      )}
+      {label && alignLeft && <FormLabel text={label} />}
     </FormGroup>
   );
 };
@@ -53,7 +71,6 @@ const styles = StyleSheet.create({
     fontSize: 16,
     padding: 8,
     flexGrow: 1,
-    textAlign: "right",
   },
 });
 
