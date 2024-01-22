22a59d6652f5c97503c1fbd9327091593b9e4742
Sadaqat Ali <46186868+SadaqatDahani786@users.noreply.github.com>
2024-01-23 02:21:20 +0500

    Added more validators such as isAlpha, isPassMissmatched and isOverEighteen. Added getFormatedDate utility function.

--- utils/validators.js
+++ utils/validators.js
@@ -28,3 +28,63 @@ export const isEmail = (val) => {
   //3) Matched failed, return true to indicate error
   return true;
 };
+
+/**
+ ** ======================================================
+ ** Validator [isAlpha]
+ ** ======================================================
+ */
+export const isAlpha = (
+  val,
+  options = {
+    ignoreSpaces: true,
+    ignoreCase: true,
+    ignoreHyphens: false,
+    ignoreDashes: false,
+    ignorePunctuations: false,
+  }
+) => {
+  //1) Regex pattern to test for alpha values
+  const pattern = `^[a-z${options.ignoreSpaces ? "\\s" : ""}${
+    options.ignoreHyphens ? "\\-" : ""
+  }${options.ignoreDashes ? "\\_" : ""}${
+    options.ignorePunctuations ? "\\-\\_\\.\\,\\\"\\'\\:\\;\\(\\)\\&\\!" : ""
+  }]+$`;
+
+  //2) Test value against regex pattern, if matched, return
+  const regex = RegExp(pattern, options.ignoreCase ? "i" : "");
+  if (regex.test(val)) return false;
+
+  //3) Matched failed, return true to indicate error
+  return true;
+};
+
+/**
+ ** ======================================================
+ ** Validator [isPassMissmatched]
+ ** ======================================================
+ */
+export const isPassMissmatched = (pass, passConfirm) => {
+  return pass !== passConfirm;
+};
+
+/**
+ ** ======================================================
+ ** Validator [isOverEighteen]
+ ** ======================================================
+ */
+export const isOverEighteen = (date) => {
+  //1) Extract day, month and year from date object
+  const day = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+
+  //2) Today's date in pure numbers only
+  const now = parseInt(new Date().toISOString().slice(0, 10).replace(/-/g, ""));
+
+  //3) Coerces strings to integers
+  const dob = year * 10000 + month * 100 + day * 1;
+
+  //4) Return the assertion
+  return now - dob >= 180000;
+};
